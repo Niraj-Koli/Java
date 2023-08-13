451589f22652d413acf1fe5c8cd2bddeaddf2054
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-13 22:01:21 +0530

    Added ArithmeticOperations File

---
+++ ArithmeticOperations/ArithmeticOperations.java
@@ -0,0 +1,18 @@
+package ArithmeticOperations;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+
+        int add = 10 + 7;
+        int sub = 10 - 7;
+        int multi = 10 * 7;
+        int div = 10 / 7;
+        int mod = 10 % 7;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(div);
+        System.out.println(mod);
+    }
+}
