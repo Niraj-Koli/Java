1e75af952d05360d9bc4ad127ec1661231f7ef4a
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-14 22:11:12 +0530

    Added DoWhileLoop File

---
+++ DoWhileLoop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package DoWhileLoop;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 10;
+
+        do {
+            System.out.println("Count = " + count);
+            count++;
+        } while (count < 17);
+    }
+}
