9f7b456b1ac7dc91028dd9cdf677286da4b21aa0
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-16 21:43:31 +0530

    Changes In ArithmeticOperations File

--- ArithmeticOperations/ArithmeticOperations.java
+++ ArithmeticOperations/ArithmeticOperations.java
@@ -8,11 +8,18 @@ public class ArithmeticOperations {
         int multi = 10 * 7;
         int div = 10 / 7;
         int mod = 10 % 7;
+        int inc = 10;
+        inc++;
+
+        int dec = 10;
+        dec--;
 
         System.out.println(add);
         System.out.println(sub);
         System.out.println(multi);
         System.out.println(div);
         System.out.println(mod);
+        System.out.println(inc);
+        System.out.println(dec);
     }
 }
