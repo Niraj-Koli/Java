dbf64198a16a6cc9885db66e5e0dd37a3f608357
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-16 19:41:22 +0530

    Added WhileLoop File

---
+++ WhileLoop/WhileLoop.java
@@ -0,0 +1,12 @@
+package WhileLoop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i < 7) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
