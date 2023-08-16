8ff9e4e9bc2a5b38dfa6a6a54a9345e7d5f47cdc
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-16 19:41:34 +0530

    Added Switch File

---
+++ Switch/Switch.java
@@ -0,0 +1,22 @@
+package Switch;
+
+public class Switch {
+    public static void main(String[] args) {
+        String x = "Nero";
+
+        switch (x) {
+            case "Kaizer":
+                System.out.println("Kaizer");
+                break;
+            case "Nero":
+                System.out.println("Nero");
+                break;
+            case "Dante":
+                System.out.println("Dante");
+                break;
+            default:
+                System.out.println("Noob");
+                break;
+        }
+    }
+}
