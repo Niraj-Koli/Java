c90f7e28ca5215298f541eb7f7c4a242e4dc8886
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-17 21:24:51 +0530

    Added Boolean File

---
+++ Boolean/Boolean.java
@@ -0,0 +1,10 @@
+package Boolean;
+
+public class Boolean {
+    public static void main(String[] args) {
+        boolean isRight = true;
+        boolean isWrong = false;
+
+        System.out.println(isRight + " x " + isWrong);
+    }
+}
