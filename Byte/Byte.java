b3c83ee55823e776f02f8f051006d72fee754813
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-17 21:25:02 +0530

    Added Byte File

---
+++ Byte/Byte.java
@@ -0,0 +1,13 @@
+package Byte;
+
+public class Byte {
+    public static void main(String[] args) {
+        byte minByte = -128;
+        byte maxByte = 127;
+
+        System.out.println("Byte (" + minByte + ", " + maxByte + ")");
+
+        byte byteNumber = 17;
+        System.out.println(byteNumber);
+    }
+}
