5fcf2795445b3dfd4dd428ff1c9dd115b5f12e02
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-17 21:25:31 +0530

    Added Char File

---
+++ Char/Char.java
@@ -0,0 +1,11 @@
+package Char;
+
+public class Char {
+    public static void main(String[] args) {
+        char myChar = 'N';
+        System.out.println("Character: " + myChar);
+
+        char mySymbol = '\u00A9';
+        System.out.println("Symbol: " + mySymbol);
+    }
+}
