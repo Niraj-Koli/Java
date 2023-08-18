98b5886f6fc291f31166535cc983da175c0f9f11
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-18 21:53:14 +0530

    Added ComparisonOperations File

---
+++ ComparisonOperations/ComparisonOperations.java
@@ -0,0 +1,12 @@
+package ComparisonOperations;
+
+public class ComparisonOperations {
+    public static void main(String[] args) {
+        System.out.println(10 == 7);
+        System.out.println(10 != 7);
+        System.out.println(10 > 7);
+        System.out.println(10 < 7);
+        System.out.println(10 >= 7);
+        System.out.println(10 <= 7);
+    }
+}
