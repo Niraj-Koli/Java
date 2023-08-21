54e3677db523dffd709ec1022e68007bdb2c90ea
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-21 23:00:25 +0530

    Added Double File

---
+++ Double/Double.java
@@ -0,0 +1,11 @@
+package Double;
+
+public class Double {
+    public static void main(String[] args) {
+        double doubleNumber = 10.2143195678236194;
+        System.out.println(doubleNumber);
+
+        double doubleValue = 21.0 / 2.0;
+        System.out.println(doubleValue);
+    }
+}
