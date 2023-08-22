343313d0497c41b39818adef955dcfbd74f34413
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-22 22:48:01 +0530

    Added Float File

---
+++ Float/Float.java
@@ -0,0 +1,11 @@
+package Float;
+
+public class Float {
+    public static void main(String[] args) {
+        float floatNumber = 10.2143195678f;
+        System.out.println(floatNumber);
+
+        float floatValue = 10f / 7f;
+        System.out.println(floatValue);
+    }
+}
