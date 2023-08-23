00f28e3027c6433f67f04c02097cda72e20d563f
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-23 23:04:29 +0530

    Added Integer File

---
+++ Integer/Integer.java
@@ -0,0 +1,14 @@
+package Integer;
+
+public class Integer {
+    public static void main(String[] args) {
+        int minInteger = -2147483648;
+        int maxInteger = 2147483647;
+
+        System.out.println("Integer (" + minInteger + ", " + maxInteger + ")");
+
+        int integerNumber = 2147;
+
+        System.out.println(integerNumber);
+    }
+}
