f3ba5160a788adde4a325da32b5c3288305fb419
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-08-25 23:53:06 +0530

    Added Long File

---
+++ Long/Long.java
@@ -0,0 +1,13 @@
+package Long;
+
+public class Long {
+    public static void main(String[] args) {
+        long minLong = -9223372036854775808L;
+        long maxLong = 9223372036854775807L;
+
+        System.out.println("Long (" + minLong + ", " + maxLong + ")");
+
+        long longNumber = 475669201L;
+        System.out.println(longNumber);
+    }
+}
