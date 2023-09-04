10134176b64893e086570c110412426ddaca9aa4
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-04 16:19:53 +0530

    Added Messages File

---
+++ Messages/Messages.java
@@ -0,0 +1,97 @@
+package Messages;
+
+import java.util.Random;
+
+public class Messages {
+    public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+    }
+}
+
+class Message {
+    private String message;
+    private boolean empty = true;
+
+    public synchronized String read() {
+        while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        empty = true;
+        notifyAll();
+
+        return message;
+    }
+
+    public synchronized void write(String message) {
+        while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        empty = false;
+        this.message = message;
+        notifyAll();
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String[] messages = {
+                "So baby pull me closer",
+                "In the back seat of your rover",
+                "That I know you can't afford",
+                "Bite that tattoo on your shoulder",
+                "Pull the sheets right of the corner",
+                "Of that mattress that you stole",
+                "From your roommate back in boulder",
+                "We ain't ever getting older",
+        };
+
+        Random random = new Random();
+
+        for (int i = 0; i < messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(3000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
