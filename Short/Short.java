1cea6206bcaeab9d65d620e807e1c38d5ee3b25a
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-10 14:10:42 +0530

    Added Short File

---
+++ Short/Short.java
@@ -0,0 +1,13 @@
+package Short;
+
+public class Short {
+    public static void main(String[] args) {
+        short minShort = -32768;
+        short maxShort = 32767;
+
+        System.out.println("Short (" + minShort + ", " + maxShort + ")");
+
+        short shortNumber = 12713;
+        System.out.println(shortNumber);
+    }
+}
